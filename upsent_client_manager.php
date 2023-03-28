92cc857a12ba0d52ec6b9194195f143973c95ca0
Buno Barreto <Buno-kk@hotmail.com>
2023-03-28 20:31:24 -0300

    fix: not an fix

--- upsent_client_manager.php
+++ upsent_client_manager.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name:     Upsent Client Manager
  * Plugin URI:      https://upsent.com.br
@@ -15,98 +16,112 @@
 use Inc\Base\Activate;
 use Inc\Base\Deactivate;
 
- if(!defined('ABSPATH')) die;
- if(file_exists(dirname(__FILE__).'/vendor/autoload.php')){
-    require_once  dirname(__FILE__).'/vendor/autoload.php';
- }
+if (!defined('ABSPATH')) die;
+if (file_exists(dirname(__FILE__) . '/vendor/autoload.php')) {
+    require_once  dirname(__FILE__) . '/vendor/autoload.php';
+}
 
 
-define('PLUGIN_PATH',plugin_dir_path(__FILE__));
-define('PLUGIN_URL',plugin_dir_url(__FILE__));
-define('PLUGIN_BASE_DIR',plugin_basename(__FILE__));
+define('PLUGIN_PATH', plugin_dir_path(__FILE__));
+define('PLUGIN_URL', plugin_dir_url(__FILE__));
+define('PLUGIN_BASE_DIR', plugin_basename(__FILE__));
 
-function activate_employer_manager_plugin(){
+function activate_employer_manager_plugin()
+{
     Activate::activate();
 }
 
-function deactivate_employer_manager_plugin(){
+function deactivate_employer_manager_plugin()
+{
     Deactivate::deactivate();
 }
 
-register_activation_hook(__FILE__,'activate_employer_manager_plugin');
-register_deactivation_hook(__FILE__,'deactivate_employer_manager_plugin');
+register_activation_hook(__FILE__, 'activate_employer_manager_plugin');
+register_deactivation_hook(__FILE__, 'deactivate_employer_manager_plugin');
 
-function get_task_data(){
+function get_task_data()
+{
     global $wpdb;
-    $table_name =$wpdb->prefix.'my_tasks';
-    $task_data=$wpdb->get_results("SELECT * FROM $table_name");
+    $table_name = $wpdb->prefix . 'my_tasks';
+    $task_data = $wpdb->get_results("SELECT * FROM $table_name");
     return $task_data;
 }
 
 
-function get_task_by_name($data){
+function get_task_by_name($data)
+{
     global $wpdb;
-    $table_name =$wpdb->prefix.'my_tasks';
-    $task_data_user=$wpdb->get_results("SELECT * FROM $table_name WHERE funcionaro_responsavel='$data'");
+    $table_name = $wpdb->prefix . 'my_tasks';
+    $task_data_user = $wpdb->get_results("SELECT * FROM $table_name WHERE funcionaro_responsavel='$data'");
     return $task_data_user;
 }
 
-function delete_task($id){
+function delete_task($id)
+{
     global $wpdb;
-    $table_name= $wpdb->prefix.'my_tasks';
+    $table_name = $wpdb->prefix . 'my_tasks';
     $wpdb->delete(
         $table_name,
-        array('id'=>$id),
+        array('id' => $id),
         array('%d')
     );
 }
 
-function update_deliverance($id,$request){
-    global $wpdb;
-    $table_name =$wpdb->prefix.'my_tasks';
-    $update_data=$wpdb->update(
-        $table_name,
-        array('entregue'=>$request->get_param('entregue')),
-        array('id'=>$id->get_param('id'))
-    );
-     return $update_data;
+function teste_route($identification)
+{
+    return 'Works' . $identification;
 }
 
-  add_action('rest_api_init',function(){
-    register_rest_route('upsent-api/v1','tasks',array(
-        'methods'=>'GET',
-        'callback'=>'get_task_data' 
-    ));
-  });
+function update_deliverance($id, $request)
+{
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'my_tasks';
+    $update_data = $wpdb->update(
+        $table_name,
+        array('entregue' => $request->get_param('entregue')),
+        array('id' => $id->get_param('id'))
+    );
+    return $update_data;
+}
 
-  add_action('rest_api_init',function(){
-    register_rest_route('upsent-api/v1','tasks/(?P<funcionario-responsavel>\w+)',array(
-        'methods'=>'GET',
-        'callback'=>'get_task_by_name' 
-    ));
-  });
 
-  add_action('rest_api_init',function(){
-    register_rest_route('upsent-api/v1','tasks/(?P<id>\d+)',array(
-        'methods'=>'DELETE',
-        'callback'=>'delete_task'
-    ));
-  });
 
-  add_action('rest_api_init',function(){
-    register_rest_route('upsent-api/v1','tasks-update/(?P<id>\d+)',array(
-        'methods'=>WP_REST_Server::CREATABLE,
-        'callback'=>'update_deliverance',
+add_action('rest_api_init', function () {
+    register_rest_route('upsent-api/v1', 'tasks', array(
+        'methods' => 'GET',
+        'callback' => 'get_task_data'
+    ));
+});
+
+add_action('rest_api_init', function () {
+    register_rest_route('upsent-api/v1', 'tasks/(?P<funcionario-responsavel>\w+)', array(
+        'methods' => 'GET',
+        'callback' => 'get_task_by_name'
+    ));
+});
+
+add_action('rest_api_init', function () {
+    register_rest_route('upsent-api/v1', 'tasks/(?P<id>\d+)', array(
+        'methods' => 'DELETE',
+        'callback' => 'delete_task'
+    ));
+});
+
+add_action('rest_api_init', function () {
+    register_rest_route('upsent-api/v1', 'tasks/(?P<identification>[\d]+)', array(
+        'methods' => WP_REST_Server::EDITABLE | WP_REST_Server::CREATABLE,
+        'callback' => 'teste_route',
         'args' => array(
-            'id' => array(
+            'identification' => array(
                 'required' => true,
-                'description' => __('The ID of the task to update', 'upsent-api'),
                 'type' => 'integer',
             ),
         ),
+        
     ));
-  });
+});
 
-if(class_exists('Inc\\Init')){
+
+if (class_exists('Inc\\Init')) {
     Inc\Init::register_services();
 }
